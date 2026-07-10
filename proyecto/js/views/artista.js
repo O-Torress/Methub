996c62b38ec9047c801f0bab586efd5b22917766
Santiago Leal <santiagoald0203@gmail.com>
2026-07-09 20:55:21 -0400

    feat: inicializar renderArtist con validacion de parametro

--- proyecto/js/views/artista.js
+++ proyecto/js/views/artista.js
@@ -1,3 +1,13 @@
-function renderArtist(container, name) {
-    container.innerHTML = '<h1>Artista: ' + (name || '') + '</h1><p>Proximamente...</p>';
+async function renderArtist(container, artistName) {
+    if (!artistName) {
+        container.innerHTML = '';
+        const errorEl = document.createElement('error-state');
+        container.appendChild(errorEl);
+        errorEl.setup('No se especificó un artista para mostrar.', () => {
+            window.location.hash = '#home';
+        });
+        return;
+    }
+
+    container.innerHTML = '';
 }
