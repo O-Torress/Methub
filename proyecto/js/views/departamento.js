1919b08876555cc10404a189565b93c9a655c7c8
Santiago Leal <santiagoald0203@gmail.com>
2026-07-09 20:11:34 -0400

    feat: agregar iconos tematicos por departamento

--- proyecto/js/views/departamento.js
+++ proyecto/js/views/departamento.js
@@ -1,3 +1,24 @@
-function renderDepartments(container) {
-    container.innerHTML = '<h1>Departamentos</h1><p>Proximamente...</p>';
+const DEPT_ICONS = {
+    1:  '🏺',
+    3:  '🏛️',
+    4:  '🔫',
+    5:  '🌿',
+    6:  '📜',
+    7:  '🎨',
+    8:  '🏺',
+    9:  '🎭',
+    10: '🌍',
+    11: '⚔️',
+    12: '🏺',
+    13: '🖼️',
+    14: '🖨️',
+    15: '🎸',
+    16: '🏛️',
+    17: '🌏',
+    18: '📷',
+    19: '🧥',
+    21: '🏺',
+};
+
+function renderDepartments(container, param) {
 }
